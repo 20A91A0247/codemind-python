7b9accff05a9e2f1d15065e558524f7d3260426f
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-06 17:10:51 +0530

    n=int(input()) l=list(map(int,input().split())) e=0 for i in range(0,len(l),2):     e=e+l[i] o=0 for i in range(1,len(l),2):     o=o+l[i] if(e-o==0):     print("YES") else:     print("NO")

---
+++ Sum_of_odd_and_even_digits.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+e=0
+for i in range(0,len(l),2):
+    e=e+l[i]
+o=0
+for i in range(1,len(l),2):
+    o=o+l[i]
+if(e-o==0):
+    print("YES")
+else:
+    print("NO")
