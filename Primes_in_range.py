e73f23cc4ae634904dbfcb3e9e4a9cdd90dbec93
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-08 20:33:07 +0530

    import math def gr(a,b):     c=0     for i in range(a,b+1):         if i>1:             for j in range(2,int(math.sqrt(i))+1):                 if i%j==0:                     break             else:                 c=c+1     print(c) a=int(input()) b=int(input()) gr(a,b)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,16 +1,14 @@
-from math import *
-def isp(n):
-    if n<=1:
-        return 0
-    else:
-        for i in range(2,int(sqrt(n))+1):
-            if n%i==0:
-                return 0
-        else:
-            return 1
-n=int(input())
-n1=int(input())
-s=0
-for i in range(n,n1+1):
-    s=s+isp(i)
-print(s)
+import math
+def gr(a,b):
+    c=0
+    for i in range(a,b+1):
+        if i>1:
+            for j in range(2,int(math.sqrt(i))+1):
+                if i%j==0:
+                    break
+            else:
+                c=c+1
+    print(c)
+a=int(input())
+b=int(input())
+gr(a,b)
