ecb3c6c9326845510b9391c8b1a01f1a27462505
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-08 20:35:46 +0530

    def isprime(n):     if n==1:         return 0     else:         for i in range(2,n):             if n%i==0:                 return 0         else:             return 1 n=int(input()) c=0 for i in range(1,n+1):     if n%i==0:         #print(i)         s=isprime(i)         #print(s)         if s==0:             c=c+1 print(c)

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -0,0 +1,19 @@
+def isprime(n):
+    if n==1:
+        return 0
+    else:
+        for i in range(2,n):
+            if n%i==0:
+                return 0
+        else:
+            return 1
+n=int(input())
+c=0
+for i in range(1,n+1):
+    if n%i==0:
+        #print(i)
+        s=isprime(i)
+        #print(s)
+        if s==0:
+            c=c+1
+print(c)
