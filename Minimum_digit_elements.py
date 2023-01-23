e8d4ed1240650b6ce3259a0234236f524fc4d1e3
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-23 22:43:58 +0530

    n=int(input()) l=list(map(str,input().split())) k=[] for i in l:     k.append(int(i)) mi=min(k) mi=str(mi) le=len(mi) #print(l,mi,le) c=0 for i in l:     if len(i)==le:         c=c+1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(str,input().split()))
+k=[]
+for i in l:
+    k.append(int(i))
+mi=min(k)
+mi=str(mi)
+le=len(mi)
+#print(l,mi,le)
+c=0
+for i in l:
+    if len(i)==le:
+        c=c+1
+print(c)
