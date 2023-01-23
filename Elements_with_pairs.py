026f3a2d85d6d7c08ff39de20f601965370fb14b
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-23 20:52:28 +0530

    #element and count(level-17) n=int(input()) ll=list(map(int,input().split())) if(n%2!=0):     ll.append(0)     for i in ll:         print(i,end=" ") else:     for i in ll:         print(i,end=" ")

---
+++ Elements_with_pairs.py
@@ -0,0 +1,10 @@
+#element and count(level-17)
+n=int(input())
+ll=list(map(int,input().split()))
+if(n%2!=0):
+    ll.append(0)
+    for i in ll:
+        print(i,end=" ")
+else:
+    for i in ll:
+        print(i,end=" ")
